a367f923cfb4d545011e7067ee6473988c64b0e9
Oleg Zaidullin <cornknight@gmail.com>
2019-09-02 23:14:56 +0600

    BinaryGap

---
+++ src/main/java/com/corn/iterations/BinaryGap.java
@@ -0,0 +1,48 @@
+package com.corn.iterations;
+
+/**
+ * A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+ * For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+ * <p>
+ * Write a function:
+ * <p>
+ * class Solution { public int solution(int N); }
+ * that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+ * <p>
+ * For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+ * <p>
+ * Write an efficient algorithm for the following assumptions:
+ * <p>
+ * N is an integer within the range [1..2,147,483,647].
+ *
+ * @author Oleg Z. (cornknight@gmail.com)
+ */
+public class BinaryGap {
+
+	public int solution(int n) {
+		int max = 0;
+
+		while (n % 2 == 0)
+			n = n >> 1;
+
+		int sum = 0;
+		while (n > 0) {
+			n = n >> 1;
+			if (n % 2 == 0) {
+				++sum;
+			} else {
+				if (sum > max)
+					max = sum;
+				sum = 0;
+			}
+		}
+
+		return max;
+	}
+
+	public static void main(String[] args) {
+		BinaryGap gap = new BinaryGap();
+		System.out.println(gap.solution(647));
+	}
+
+}
