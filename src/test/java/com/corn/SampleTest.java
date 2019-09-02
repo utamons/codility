75fe14e9e88fcf7ec4ef1d561d81d663ef9e97bd
Oleg Zaidullin <cornknight@gmail.com>
2019-09-02 22:37:50 +0600

    a sceleton

---
+++ src/test/java/com/corn/SampleTest.java
@@ -0,0 +1,18 @@
+package com.corn;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class SampleTest {
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
